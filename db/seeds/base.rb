b48e83d593b8c8557833b9a24705d202870767d1
Automate Café <izoret@kla.us>
2025-10-30 11:20:00 +0100

    add seed file handling

---
+++ db/seeds/base.rb
@@ -0,0 +1,25 @@
+# This file should ensure the existence of records required to run the application in every environment (production,
+# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
+# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
+#
+# Example:
+#
+#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
+#     MovieGenre.find_or_create_by!(name: genre_name)
+#   end
+
+etu = ProSituation.find_or_create_by!(content: "Étudiant")
+
+m = Sex.find_or_create_by!(value: "M")
+f = Sex.find_or_create_by!(value: "F")
+
+o_inc = Origin.find_or_create_by!(content: "Inconnue")
+
+Person.create(
+  quickname: "test",
+  first_name: FirstName.find_or_create_by!(content: "testP"),
+  last_name: LastName.find_or_create_by!(content: "testN"),
+  sex: m,
+  pro_situation: etu,
+  origin: o_inc
+)
