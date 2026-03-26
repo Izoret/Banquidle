430ad966c1c97c9fea8c9c3db7779ab96bea8c8e
raphael.izoret <raphael.izoret@etu.umontpellier.fr>
2026-03-26 10:36:41 +0100

    prevent Bot from sending invite on the week-end

--- Bot/src/config.js
+++ Bot/src/config.js
@@ -9,7 +9,7 @@ export const C = {
     GUILD_ID: process.env.GUILD_ID,
     TEXT_CHANNEL_ID: process.env.TEXT_CHANNEL_ID,
     PATCH_NOTES_DIR: path.resolve('res/patches'),
-    CRON_SCHEDULE_START: '12 08 * * *',
+    CRON_SCHEDULE_START: '12 08 * * 1-5',
     CRON_RANDOM_WAIT_MAX_MINUTES: 89,
     TIMEZONE: 'Europe/Paris'
 }
