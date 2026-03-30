4bfc11a96feadee713dfad0dee5532eb6c6cb2a4
raphael.izoret <raphael.izoret@etu.umontpellier.fr>
2026-03-30 08:37:43 +0200

    last perf fix

--- Bot/src/services/apiService.js
+++ Bot/src/services/apiService.js
@@ -1,6 +1,6 @@
 export async function fetchDailyTries() {
     try {
-        const url = 'http://webapp/api/v0/last_performance'
+        const url = 'http://webapp:3000/api/v0/last_performance'
         const response = await fetch(url)
         if (!response.ok) throw new Error(`HTTP error! status: ${response.status}`)
 
