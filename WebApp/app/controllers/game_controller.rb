045a06e87c3e08a6b20ee1d18adc7973e3ef080a
raphael.izoret <raphael.izoret@etu.umontpellier.fr>
2026-07-03 15:38:40 +0200

    implement system of 5 unselectable people different each day

--- WebApp/app/controllers/game_controller.rb
+++ WebApp/app/controllers/game_controller.rb
@@ -1,13 +1,14 @@
 class GameController < ApplicationController
   skip_before_action :verify_authenticity_token, only: [ :submit_guess ]
-  before_action :get_todays_person, :require_auth
+  before_action :require_auth
 
   def load_content
     previous_guesses = GameStatsService.todays_guesses @user_id
     previous_people = Person.where quickname: previous_guesses
     previous_quicknames = previous_guesses.map { |q| previous_people.find { |p| p.quickname == q } }.compact.reverse
 
-    available_people = Person.where.not(quickname: previous_guesses).order(:quickname)
+    todays = TodaysPersonService.get_daily_target
+    available_people = Person.where.not(quickname: previous_guesses).where.not(quickname: TodaysPersonService.get_daily_banned_quicknames).order(:quickname)
 
     respond_to do |format|
       format.turbo_stream do
@@ -18,8 +19,8 @@ class GameController < ApplicationController
             nb_tries: previous_guesses.length,
             available_people: available_people,
             previous_picks: previous_quicknames,
-            won: previous_quicknames.include?(@todays_person),
-            todays_person: @todays_person
+            won: previous_quicknames.include?(todays),
+            todays_person: todays
           }
         )
       end
@@ -29,10 +30,12 @@ class GameController < ApplicationController
   def submit_guess
     guesses = GameStatsService.todays_guesses @user_id
 
+    todays = TodaysPersonService.get_daily_target
+
     person = Person.find_by quickname: params[:quickname]
 
     respond_to do |format|
-      if guesses.include? @todays_person.quickname
+      if guesses.include? todays.quickname
         flash.now[:error] = "Partie terminée"
         format.turbo_stream { render turbo_stream: turbo_stream.replace("flash", partial: "layouts/flash") }
 
@@ -44,13 +47,17 @@ class GameController < ApplicationController
         flash.now[:alert] = "Déjà essayé ! 🐒"
         format.turbo_stream { render turbo_stream: turbo_stream.replace("flash", partial: "layouts/flash") }
 
+      elsif TodaysPersonService.get_daily_banned_quicknames.include? person.quickname
+        flash.now[:alert] = "Bah chef il est ban ajd lui 🐒"
+        format.turbo_stream { render turbo_stream: turbo_stream.replace("flash", partial: "layouts/flash") }
+
       else
         GameStatsService.add_guess(@user_id, person.quickname)
 
         format.turbo_stream do
           render locals: {
             guessed_person: person,
-            todays_person: @todays_person,
+            todays_person: todays,
             nb_tries: guesses.length + 1,
             available_people_left: Person.where.not(quickname: (GameStatsService.todays_guesses @user_id)).order(:quickname)
           }
@@ -60,10 +67,6 @@ class GameController < ApplicationController
   end
 
   private
-
-  def get_todays_person
-    @todays_person = TodaysPersonService.get_daily
-  end
 
   def require_auth
     @user_id = params[:user_id]
--- WebApp/app/services/todays_person_service.rb
+++ WebApp/app/services/todays_person_service.rb
@@ -1,28 +1,50 @@
 class TodaysPersonService
-  def self.get_daily
-    if (person = born_today)
-      return person
+  class << self
+    def get_daily_target
+      if (person = born_today)
+        return person
+      end
+
+      Person.find_by quickname: get_target(Date.today)
     end
 
-    todays = get_quickname_of Date.today
+    def get_daily_banned_quicknames
+      get_banned(Date.today, 5)
+    end
 
-    Person.find_by quickname: todays
-  end
+    private
 
-  def self.get_determinist
-    Person.find_by(quickname: "Louane")
-  end
+    def get_target(date)
+      quicknames = Person.order(:quickname).pluck(:quickname)
+      md5_hex = Digest::MD5.hexdigest(date.to_s)
+      md5_decimal = md5_hex.to_i 16
 
-  private
+      quicknames[md5_decimal % quicknames.size]
+    end
 
-  def self.get_quickname_of(date)
-    quicknames = Person.order(:quickname).pluck(:quickname)
-    hex = Digest::MD5.hexdigest(date.to_s)
-    idx = hex.to_i(16) % quicknames.size
-    quicknames[idx]
-  end
+    def get_banned(date, number_of_banned)
+      quicknames = Person.order(:quickname).pluck(:quickname)
+      md5_hex = Digest::MD5.hexdigest date.to_s
+      md5_decimal = md5_hex.to_i 16
 
-  def self.born_today
-    Person.where(birth_day: Date.today.day, birth_month: Date.today.month).sample
+      step = 10000
+      banned = []
+
+      1.upto(number_of_banned) do |n|
+        if md5_decimal == 0
+          raise "You have banned to many people for this size of a md5 decimal bro"
+          # yes this is stupid. but it's hand-written :D
+        end
+        idx = md5_decimal % step
+        banned << quicknames.delete_at(idx % quicknames.size)
+        md5_decimal /= step
+      end
+
+      banned
+    end
+
+    def born_today
+      Person.where(birth_day: Date.today.day, birth_month: Date.today.month).sample
+    end
   end
 end
